f8edec84035a9b0a3a6119acb586ca0b92a85f87
hughfb99 <hughblakemore@hotmail.com>
2021-03-09 21:09:36 -0800

    Created my script

--- Analysis/Hugh Blakemore/scripts/project_functions.py
+++ Analysis/Hugh Blakemore/scripts/project_functions.py
@@ -1,1 +1,13 @@
-
+def load_and_process(url):
+    df1 = (
+          pd.read_csv(url)
+          .rename({'children':'Dependents'},axis=1)
+          .dropna(subset=['charges'])
+          .replace({'southwest':'SW','southeast':'SE','northeast':'NE','northwest':'NW'})
+      )
+    df2=(df1
+         .round({"charges":2,"bmi":1})
+         .sort_values('charges',ascending=True)
+         .reset_index(drop=True)  
+    )
+    return df2
