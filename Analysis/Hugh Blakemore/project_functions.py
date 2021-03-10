a5484ef00e79e6993b42b565b0e75aad8f2f5a88
hughfb99 <77305978+hughfb99@users.noreply.github.com>
2021-03-10 09:12:10 -0800

    Add files via upload
    symbolic link to functions only way i could get this to worj

---
+++ Analysis/Hugh Blakemore/project_functions.py
@@ -0,0 +1,15 @@
+
+def load_and_process(url):
+    import pandas as pd
+    df1 = (
+          pd.read_csv(url)
+          .rename({'children':'Dependents'},axis=1)
+          .dropna(subset=['charges'])
+          .replace({'southwest':'SW','southeast':'SE','northeast':'NE','northwest':'NW'})
+      )
+    df2=(df1
+         .round({"charges":2,"bmi":1})
+         .sort_values('charges',ascending=True)
+         .reset_index(drop=True)  
+    )
+    return df2
